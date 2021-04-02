f9004134e7a9eb9d51d2aa1688d3057fe844c3a3
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-04-02 16:48:32 -0700

    Removed MAVROS specific stuff

--- src/server_connector/src/connection_manager.py
+++ src/server_connector/src/connection_manager.py
@@ -4,8 +4,7 @@ import rospy
 import roslaunch
 import os
 from topic_publisher import TopicPublisher
-from std_srvs.srv import Trigger, TriggerResponse
-from mavros_msgs.msg import State
+from std_msgs.msg import UInt8
 
 class ConnectionManager:
 
@@ -17,19 +16,19 @@ class ConnectionManager:
         self.server_connection = None
         self.topic_publisher = None
         self.persist_connection = True
-        self.launched_mavros = False
-        self.drone_state = None
+        self.launched_dji = False
+        self.flight_status = None
         rospy.Service('shutdown', Trigger, self.shutdown_service_callback)
 
     def register_drone(self, client, drone_name):
         service = roslibpy.Service(client, '/isaacs_server/register_drone', 'isaacs_server/register_drone')
-        request = roslibpy.ServiceRequest({'drone_name': drone_name, 'drone_type': 'Mavros'})
+        request = roslibpy.ServiceRequest({'drone_name': drone_name, 'drone_type': 'DjiMatrice'})
 
         result = service.call(request)
         rospy.loginfo(result)
         return result
 
-    def launch_mavros(self):
+    def launch_dji_sdk(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         print(dir_path)
         new_path = os.path.join(dir_path, '../launch/drone.launch')
@@ -48,20 +47,20 @@ class ConnectionManager:
             self.server_connection.run(timeout=5)
         except Exception:
             rospy.logerr('Unable to connect to server!')
-        if not self.launched_mavros:
+        if not self.launched_dji:
             if self.server_connection.is_connected:
                 result = self.register_drone(self.server_connection, self.name)
                 self.namespace = "/drone_" + str(result['id'])
-                rospy.loginfo('Connected! Launching MAVRos!')
-                self.launch_mavros()
-                self.launched_mavros = True
+                rospy.loginfo('Connected! Launching DJI SDK!')
+                self.launch_dji_sdk()
+                self.launched_dji = True
                 self.topic_publisher = TopicPublisher(self.namespace, self.server_connection)
 
                 # Starts Listening in For The Drone State (To Determine if Shutdowns are OK)
-                rospy.Subscriber(self.namespace + '/mavros/state', State, self.state_subscriber_callback)
+                rospy.Subscriber(self.namespace + '/dji_sdk/flight_status', UInt8, self.state_subscriber_callback)
 
     def state_subscriber_callback(self, data):
-        self.drone_state = data
+        self.flight_status = data
 
     def check_connection(self):
         if self.persist_connection:
@@ -79,7 +78,8 @@ class ConnectionManager:
     def shutdown_service_callback(self, req):
         success = False
         message = 'Unable to shutdown drone. May be armed.'
-        if self.drone_state and not self.drone_state.armed:
+        # STATUS 1 means the drone is on the ground
+        if self.flight_status and self.flight_status.data == 1:
             success = True
             message = "Shutting down drone."
             rospy.Timer(rospy.Duration(1.5), self.stop_connection_event, oneshot=True)
--- src/server_connector/src/topic_publisher.py
+++ src/server_connector/src/topic_publisher.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 import rospy
 import roslibpy
-from mavros_msgs.srv import SetMode
-from mavros_msgs.srv import WaypointPush
-from mavros_msgs.msg import Waypoint
-from sensor_msgs.msg import NavSatFix
-from mavros_msgs.srv import CommandBool
-from mavros_msgs.srv import CommandTOL
-from mavros_msgs.srv import WaypointClear
 from std_srvs.srv import Trigger
 
 class TopicPublisher:
@@ -21,21 +14,8 @@ class TopicPublisher:
         self.advertise_basic_services_topics()
 
     def advertise_basic_services_topics(self):
-        self.advertise_service('/mavros/set_mode', 'mavros_msgs/SetMode', self.set_mode, include_namespace=True)
-        self.advertise_service('/mavros/mission/push', 'mavros_msgs/WaypointPush',
-                               self.mission_waypoint_push, include_namespace=True)
-        self.advertise_service('/mavros/cmd/arming', 'mavros_msgs/CommandBool',
-                               self.arm, include_namespace=True)
-        self.advertise_service('/mavros/cmd/takeoff', 'mavros_msgs/CommandTOL',
-                               self.takeoff, include_namespace=True)
-        self.advertise_service('/mavros/mission/clear', 'mavros_msgs/WaypointClear',
-                               self.waypoint_clear, include_namespace=True)
-        self.advertise_service('/mavros/cmd/land', 'mavros_msgs/CommandTOL',
-                               self.land_drone, include_namespace=True)
         self.advertise_service('/shutdown', 'std_srvs/Trigger'
                                , self.shutdown, include_namespace=True)
-        self.publish_topic('/mavros/global_position/global', 'sensor_msgs/NavSatFix', NavSatFix
-                           , self.gps_publisher, include_namespace=True)
 
     def get_topics(self):
         return rospy.get_published_topics(self.namespace)
@@ -61,94 +41,22 @@ class TopicPublisher:
         rospy.Subscriber(topic_name, topic_type_class, publish)
 
     # ALL PUBLISHERS
-    def gps_publisher(self, publisher, data):
-        publisher.publish({'latitude': data.latitude, 'longitude': data.longitude, 'altitude': data.altitude})
-
-        # DONE FOR TAKEOFF LOCATION
-        self.location = data
 
     # ALL SERVICE CALLBACKS
-    def set_mode(self, request, response):
+    def set_speed(self, request, response):
         rospy.loginfo(request)
-        set_mode_service = rospy.ServiceProxy(self.namespace + '/mavros/set_mode', SetMode)
-        local_response = set_mode_service(request.get('basic_mode'), request.get('custom_mode'))
-        response['mode_sent'] = True
+        set_speed_service = rospy.ServiceProxy(self.namespace + '/dji_sdk/mission_waypoint_setSpeed', MissionWpSetSpeed)
+        local_response = set_speed_service(request.get('basic_mode'), request.get('custom_mode'))
+        response['mode_sent'] = local_response.mode_sent
         rospy.loginfo(local_response.mode_sent)
         return True
 
-    def mission_waypoint_push(self, request, response):
+    def get_speed(self, request, response):
         rospy.loginfo(request)
-        mission_push_service = rospy.ServiceProxy(self.namespace + '/mavros/mission/push', WaypointPush)
-        waypoints = []
-        for i in range(2):
-            takeoff_waypoint = Waypoint()
-            takeoff_waypoint.command = 22
-            takeoff_waypoint.param1 = 0
-            takeoff_waypoint.param2 = 0
-            takeoff_waypoint.param3 = 0
-            takeoff_waypoint.param4 = 0
-            if self.location:
-                takeoff_waypoint.x_lat = self.location.latitude
-                takeoff_waypoint.y_long = self.location.longitude
-                takeoff_waypoint.z_alt = 10
-            takeoff_waypoint.is_current = True
-            takeoff_waypoint.autocontinue = True
-            takeoff_waypoint.frame = 3
-            waypoints.append(takeoff_waypoint)
-        for waypoint in request.get('waypoints'):
-            new_waypoint = Waypoint()
-            new_waypoint.command = waypoint.get('command')
-            new_waypoint.is_current = waypoint.get('is_current')
-            new_waypoint.autocontinue = waypoint.get('autocontinue')
-            new_waypoint.param1 = waypoint.get('param1')
-            new_waypoint.param2 = waypoint.get('param2')
-            new_waypoint.param3 = waypoint.get('param3')
-            new_waypoint.param4 = waypoint.get('param4')
-            new_waypoint.x_lat = waypoint.get('x_lat')
-            new_waypoint.y_long = waypoint.get('y_long')
-            new_waypoint.z_alt = waypoint.get('z_alt')
-            new_waypoint.frame = waypoint.get('frame')
-            waypoints.append(new_waypoint)
-        rospy.loginfo(waypoints)
-        local_response = mission_push_service(request.get('start_index'), waypoints)
-        response['success'] = local_response.success
-        response['wp_transfered'] = local_response.wp_transfered
-        return local_response.success
-
-    def arm(self, request, response):
-        rospy.loginfo(request)
-        arm_service = rospy.ServiceProxy(self.namespace + '/mavros/cmd/arming', CommandBool)
-        local_response = arm_service(request.get('value'))
-        response['success'] = local_response.success
-        rospy.loginfo(local_response.success)
-        return True
-
-    def takeoff(self, request, response):
-        rospy.loginfo(request)
-        takeoff_service = rospy.ServiceProxy(self.namespace + '/mavros/cmd/takeoff', CommandTOL)
-        local_response = takeoff_service(request.get('min_pitch'), request.get('yaw'),
-                                         request.get('latitude'), request.get('longitude'),
-                                         request.get('altitude'))
-        response['success'] = local_response.success
-        rospy.loginfo(local_response.success)
-        return True
-
-    def waypoint_clear(self, request, response):
-        rospy.loginfo(request)
-        waypoint_clear_service = rospy.ServiceProxy(self.namespace + '/mavros/mission/clear', WaypointClear)
-        local_response = waypoint_clear_service()
-        response['success'] = local_response.success
-        rospy.loginfo(local_response.success)
-        return True
-
-    def land_drone(self, request, response):
-        rospy.loginfo(request)
-        land_service = rospy.ServiceProxy(self.namespace + '/mavros/cmd/land', CommandTOL)
-        local_response = land_service(request.get('min_pitch'), request.get('yaw'),
-                                      request.get('latitude'), request.get('longitude'),
-                                      request.get('altitude'))
-        response['success'] = local_response.success
-        rospy.loginfo(local_response.success)
+        get_speed_service = rospy.ServiceProxy(self.namespace + '/dji_sdk/mission_waypoint_getSpeed', MissionWpGetSpeed)
+        local_response = get_speed_service(request.get('basic_mode'), request.get('custom_mode'))
+        response['mode_sent'] = local_response.mode_sent
+        rospy.loginfo(local_response.mode_sent)
         return True
 
     def shutdown(self, request, response):
